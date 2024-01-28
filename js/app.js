693d3ae1c241105b6b8c1224bcd0a2079f33206a
hamedbed <hamed.b99@gmail.com>
2024-01-28 11:19:44 +0330

    12th edit

--- js/app.js
+++ js/app.js
@@ -12,5 +12,35 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+    var instances = M.Sidenav.init(elems, {edge:right});
   });
+
+
+
+
+  // console.log(1);
+  // new Promise(
+  //   function(resolve, reject){
+  //     setTimeout(function(){
+  //       console.log(2);
+  //       resolve("ok")
+        
+  //     }, 2000);
+  //   }
+  //   ).then(
+  //     function(response){
+  //       console.log(response);
+  //       console.log(3);
+  //     }
+  //   ).catch(
+  //     function(error){
+  //       console.log(error);
+  //     }
+  //   )
+
+
+
+  fetch('https://jsonplaceholder.typicode.com/posts/users')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+  
