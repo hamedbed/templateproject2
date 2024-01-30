01f4461b88cfb47b7e0eaf8bb6e56fbd10c28179
hamedbed <hamed.b99@gmail.com>
2024-01-30 18:07:31 +0330

    18th edit

--- js/alpinejscontrollers/usercontrollers.js
+++ js/alpinejscontrollers/usercontrollers.js
@@ -2,12 +2,17 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('usersdata',function(){
         return{
             users:[],
+            pageusers:[],
             isloading: false,
             showaddmodal: false,
+            pagecount:1,
+            itemscount:4,
+            currentpage:1,
             getusers(){ 
                 this.isloading= true
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                      this.users=res.data
+                     this.pagination()
                  }).catch(error=>
                  {
                     console.log(error.message)
@@ -16,6 +21,28 @@ document.addEventListener('alpine:init', () => {
                     this.isloading=false
 
                  })
+            },
+            pagination()
+            {
+                
+                this.pagecount= Math.ceil(this.users.length/this.itemscount)
+                let start=(this.currentpage*this.itemscount)-this.itemscount
+                let end= this.currentpage* this.itemscount
+                this.pageusers=this.users.slice(start,end)
+            },
+            nextpage()
+            {
+                this.currentpage++
+                if (this.currentpage>this.pagecount)    this.currentpage=this.pagecount
+                
+                this.pagination()
+            },
+            priviouspage()
+            {
+                this.currentpage--
+                if (this.currentpage<1)    this.currentpage=1
+                
+                this.pagination()
             }
         }
     })
